8ca476238d89266be5fa9fa3ef784b0f975b8c6b
Arijit Basu <sayanarijit@gmail.com>
2019-01-01 09:20:52 -0800

    python 3.7 cant be tested as of now

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        # 'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Utilities',
         'Topic :: Software Development',
