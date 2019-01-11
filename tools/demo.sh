5015f1126949fc3663180cf508ce6b391b2bf716
Surya Teja Valluri <SV00483947@techmahindra.com>
2019-01-11 16:07:41 +0530

    minimized command

--- tools/demo.sh
+++ tools/demo.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-clear
-echo '========================================='
-echo '* Checking python compatibility *'
-echo '========================================='
-if (which python3); then 
-    echo "Python3 is available"; 
-else 
-    echo "unable to find to python3. Make sure that python3 is installed and exported to PATH.";
-    exit 1
-fi
-echo
+which python3 > /dev/null || exit 1
 
+clear
 echo '========================================='
 echo '* Starting RESTEasyCLI interactive demo *'
 echo '========================================='
@@ -24,9 +15,9 @@ echo
 echo
 
 echo '# Create and activate virtual environment'
-read -sp '> virtualenv --python=$(which python3) ~/recli_demo_venv' x
+read -sp '> virtualenv -p python3 ~/recli_demo_venv' x
 echo
-virtualenv ~/recli_demo_venv || exit 1
+virtualenv -p python3 ~/recli_demo_venv || exit 1
 echo
 
 read -sp '> source ~/recli_demo_venv/bin/activate' x
